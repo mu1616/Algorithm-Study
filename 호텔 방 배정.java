0d5d39ffbdfa35d445e0ea784272d8fa81a2827a
mu1616 <mu1616@naver.com>
2020-05-07 05:20:59 +0900

    Create 호텔 방 배정.java
    2019 카카오 개발자 겨울 인턴쉽 코딩테스트 4번문제
    //정확성 통과, 효율성 실패//
    //다시 풀어보자..

---
+++ 호텔 방 배정.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    static HashMap<Long,Long> map = new HashMap<>();
+	
+	public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+        for(int i=0; i< room_number.length; i++) {
+        	if(map.containsKey(room_number[i])) {
+        		long node = find(room_number[i]);
+        		map.put(node, node+1);
+        		answer[i] = node;
+        	}else {      	
+        		map.put(room_number[i],room_number[i]+1);
+        		answer[i] = room_number[i];
+        	}
+        }
+        return answer;
+    }
+    
+	public long find(long i) {
+		while(true) {
+			if(map.containsKey(i)) {
+				i++;
+			}else {
+                return i;
+			}
+		}
+	}
+}
