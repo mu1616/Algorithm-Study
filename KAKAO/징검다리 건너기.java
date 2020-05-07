a8b4a5a15b9639867fe9d3d0fddc1d3fca2bf964
mu1616 <mu1616@naver.com>
2020-05-08 03:48:20 +0900

    징검다리건너기 나의풀이, 폴더정리
    2019 카카오 개발자 겨울 인턴쉽 5번 징검다리건너기
    //정확성 통과, 효율성 실패
    //다시 풀어보자

---
+++ KAKAO/징검다리 건너기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class Solution {
+
+	HashMap<Integer, Integer> map = new HashMap<>();
+	public int solution(int[] stones, int k) {
+		int answer = 0;
+		// map.put(stones.length - 1, -1);
+		boolean complete = true;
+		while (true) {
+			for (int i = 0; i < stones.length; i++) {
+				if (map.containsKey(i)) {
+					int diff = map.get(i) - i;
+					if (diff >= k) {
+						complete = false;
+					} else {
+						i = i + (diff - 1);
+						continue;
+					}
+				}
+				stones[i] = stones[i] - 1;
+				if (stones[i] == 0) {
+					linkNode(i, stones);
+				}
+			}
+			if (complete == false)
+				break;
+			if (complete == true)
+				answer++;
+		}	
+		return answer;
+	}
+
+	void linkNode(int n, int[] stones) {
+		// 오른쪽 검사
+
+		if (map.containsKey(n + 1)) {
+			map.put(n, map.get(n + 1));
+		}
+		// 왼쪽 검사
+		int destNode;
+		if(map.containsKey(n)) {
+			destNode = map.get(n);
+		} else {
+			destNode = n+1;
+		}
+		while (true) {
+			if (n!=0 && stones[n-1]==0) {
+				n--;
+			} else {
+				break;
+			}
+		}
+		map.put(n, destNode);
+	}
+}
