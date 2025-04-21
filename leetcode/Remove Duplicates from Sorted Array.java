6b3c5a277912cf82c5da5a91e4380f20b5735436
mu1616 <mu1616@naver.com>
2025-04-21 18:10:35 +0900

    leetcode/Remove Duplicates from Sorted Array

---
+++ leetcode/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
