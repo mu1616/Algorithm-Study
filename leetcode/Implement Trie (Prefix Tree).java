737cd484016f1a03bd40ebca28083ee065ef5f6f
mu1616 <mu1616@naver.com>
2025-07-05 22:08:13 +0900

    leetcode/Implement Trie (Prefix Tree)

---
+++ leetcode/Implement Trie (Prefix Tree).java
@@ -0,0 +1,49 @@
+class Trie {
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            node.children.putIfAbsent(c, new TrieNode());
+            node = node.children.get(c);
+        }
+        node.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return node.isEndOfWord();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return true;
+    }
+
+    public static class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+
+        boolean endOfWord = false;
+
+        public boolean isEndOfWord() {
+            return endOfWord;
+        }
+    }
+}
